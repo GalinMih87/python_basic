b7a95895496433c25c577d87c6ae979b49d04fc5
Galin Mihaylov <101407321+GalinMih87@users.noreply.github.com>
2023-06-20 15:16:55 +0300

    Add files via upload

---
+++ first_steps_in_coding_lab/projects_creation.py
@@ -0,0 +1,6 @@
+name_on_architect = input()
+number_of_projects = int(input())
+
+need_hours = number_of_projects * 3
+
+print(f'The architect {name_on_architect} will need {need_hours} hours to complete {number_of_projects} project/s.')
