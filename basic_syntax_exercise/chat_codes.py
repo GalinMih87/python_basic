0e7571c8539567504938405d37b8ef06c2522ee4
Galin Mihaylov <101407321+GalinMih87@users.noreply.github.com>
2023-06-21 09:51:48 +0300

    Add files via upload

---
+++ basic_syntax_exercise/chat_codes.py
@@ -0,0 +1,13 @@
+number = int(input())
+
+for number in range(number):
+    number = int(input())
+
+    if number == 88:
+        print("Hello")
+    elif number == 86:
+        print("How are you?")
+    elif number < 88:
+        print("GREAT!")
+    elif number > 88:
+        print("Bye.")
