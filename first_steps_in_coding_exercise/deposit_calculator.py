ed41b84975d967effcd3cb17b1719cf46cc13248
Galin Mihaylov <101407321+GalinMih87@users.noreply.github.com>
2023-06-22 10:16:44 +0300

    Add files via upload

---
+++ first_steps_in_coding_exercise/deposit_calculator.py
@@ -0,0 +1,11 @@
+depositet_amount = float(input())
+pay_time = int(input())
+annual_interest = float(input())
+
+accrued_interest = depositet_amount * (annual_interest / 100)
+interest_per_month = (accrued_interest / 12)
+total = depositet_amount + (pay_time * interest_per_month)
+
+print(total)
+
+
