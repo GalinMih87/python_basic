c58463a0e8de83d00a692e04f18cda278bd7f6f8
Galin Mihaylov <101407321+GalinMih87@users.noreply.github.com>
2023-06-22 10:22:35 +0300

    Update deposit_calculator.py

--- first_steps_in_coding_exercise/deposit_calculator.py
+++ first_steps_in_coding_exercise/deposit_calculator.py
@@ -5,7 +5,5 @@ annual_interest = float(input())
 accrued_interest = depositet_amount * (annual_interest / 100)
 interest_per_month = (accrued_interest / 12)
 total = depositet_amount + (pay_time * interest_per_month)
-
-print(total)
-
-
+formatted_total = round(total, 2)
+print(formatted_total)
