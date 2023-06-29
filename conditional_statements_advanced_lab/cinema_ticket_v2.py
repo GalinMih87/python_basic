0e86db37d0581f26b6debb05467597d6748aeddd
Galin Mihaylov <101407321+GalinMih87@users.noreply.github.com>
2023-06-29 09:49:39 +0300

    Add files via upload

---
+++ conditional_statements_advanced_lab/cinema_ticket_v2.py
@@ -0,0 +1,12 @@
+day_of_the_week = input()
+ticket_price = 12
+
+if day_of_the_week == "Wednesday" :
+    ticket_price = 14
+elif day_of_the_week == "Thursday" :
+    ticket_price = 14
+elif day_of_the_week == "Saturday" :
+    ticket_price = 16
+elif day_of_the_week == "Sunday" :
+    ticket_price = 16
+print(ticket_price)
