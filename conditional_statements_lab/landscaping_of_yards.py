3bbc82cdddaadde0d52a92985a9b172f6a714b0f
Galin Mihaylov <101407321+GalinMih87@users.noreply.github.com>
2023-07-03 09:42:46 +0300

    Add files via upload

---
+++ conditional_statements_lab/landscaping_of_yards.py
@@ -0,0 +1,10 @@
+square_meters = float(input())
+
+price = square_meters * 7.61
+discount = price * 0.18
+price_with_discount = price - discount
+
+print(f'The final price is: {price_with_discount} lv.')
+print(f'The discount is: {discount} lv.')
+
+
