80a469acff71d01cf271563be26dc65547954cf2
Galin Mihaylov <101407321+GalinMih87@users.noreply.github.com>
2023-07-10 14:38:25 +0300

    Add files via upload

---
+++ nested_loops_lab/sum_of_two_numbers.py
@@ -0,0 +1,20 @@
+start = int(input())
+end = int(input())
+magic_numb = int(input())
+
+flag = False
+count = 0
+for i in range(start, end + 1):
+    for j in range(start, end + 1):
+        sum = i + j
+        count += 1
+
+        if sum == magic_numb:
+            print(f"Combination N:{count} ({i} + {j} = {sum})")
+            flag = True
+            break
+    if flag:
+        break
+
+if not flag:
+    print(f"{count} combinations - neither equals {magic_numb}")
