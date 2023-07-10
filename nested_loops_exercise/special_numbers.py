9850b75dbbc423dd7dcfa4ee61eb72c16f38a465
Galin Mihaylov <101407321+GalinMih87@users.noreply.github.com>
2023-07-10 14:30:19 +0300

    Add files via upload

---
+++ nested_loops_exercise/special_numbers.py
@@ -0,0 +1,10 @@
+number = int(input())
+for curren_number in range(1111, 9999 + 1):
+    number_is_special = True
+    curren_number_as_string = str(curren_number)
+    for curren_digit in curren_number_as_string:
+        if int(curren_digit) == 0 or number % int(curren_digit) != 0:
+            number_is_special = False
+            break
+    if number_is_special:
+        print(curren_number_as_string, end=" ")
