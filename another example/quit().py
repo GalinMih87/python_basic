00385208d3ed5247f0141c09760c6e5149f3ab8f
Galin Mihaylov <101407321+GalinMih87@users.noreply.github.com>
2023-07-12 10:11:02 +0300

    Add files via upload

---
+++ another example/quit().py
@@ -0,0 +1,10 @@
+for i in range(10):
+
+    # If the value of i becomes
+    # 5 then the program is forced
+    # to quit
+    if i == 5:
+        # prints the quit message
+        print(quit)
+        quit()
+    print(i)
