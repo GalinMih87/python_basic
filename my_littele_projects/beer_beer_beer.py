25996eb608ae4b19aa0f8e6fe8dfafd78492fd3d
Galin Mihaylov <101407321+GalinMih87@users.noreply.github.com>
2023-07-13 13:15:07 +0300

    Update beer_beer_beer.py
    I add free space betwеen text and Unicode Character.

--- my_littele_projects/beer_beer_beer.py
+++ my_littele_projects/beer_beer_beer.py
@@ -2,5 +2,5 @@ n = 0
 
 while n < 3:
     n +=1
-    print("Beer" "\N{Beer Mug}")
-print("It's beer time" "\N{Clinking Beer Mugs}")
+    print("Beer:" " \N{Beer Mug}")
+print("It's beer time:" " \N{Clinking Beer Mugs}")
