10a8444c8b407cce91335c0733200cbca15039f9
Galin Mihaylov <101407321+GalinMih87@users.noreply.github.com>
2023-07-14 10:25:22 +0300

    Add files via upload

---
+++ my_littele_projects/os._exit(n).py
@@ -0,0 +1,21 @@
+import os
+
+pid = os.fork()
+
+if pid > 0:
+
+    print("\nIn parent process")
+
+    info = os.waitpid(pid, 0)
+
+    if os.WIFEXITED(info[1]):
+        code = os.WEXITSTATUS(info[1])
+        print("Child's exit code:", code)
+
+else:
+    print("In child process")
+    print("Process ID:", os.getpid())
+    print("Hello ! Geeks")
+    print("Child exiting..")
+
+    os._exit(os.EX_OK)
